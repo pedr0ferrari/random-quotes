1ae95f8d775ceac21dcc7457466083161b87b99b
pedr0ferrari <pedrosantanaferrari96@gmail.com>
2023-01-31 14:56:53 -0300

    feat: add random quote card and random quote button function

---
+++ interface/QuoteType.ts
@@ -0,0 +1,5 @@
+export interface QuoteType {
+  id: number;
+  quote: string;
+  author: string;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,36 @@
 import Head from "next/head";
-import { Inter } from "@next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import Navbar from "@/components/Navbar";
+import { Quotecard } from "@/components/Quotecard";
+import { getQuotes } from "@/lib/getQuotes";
+import { QuoteType } from "@/interface/QuoteType";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [quotes, setQuotes] = useState<QuoteType[]>([]);
+  const [currentQuote, setCurrentQuote] = useState<QuoteType | null>(null);
+
+  const handleGetQuotes = async () => {
+    const quotes = await getQuotes();
+    setQuotes(quotes);
+  };
+
+  const handleCurrentQuote = (quotes: QuoteType[]) => {
+    const currentQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+    return currentQuote;
+  };
+
+  useEffect(() => {
+    handleGetQuotes();
+  }, []);
+
+  useEffect(() => {
+    if (quotes.length > 0) {
+      const currentQuote = handleCurrentQuote(quotes);
+      setCurrentQuote(currentQuote);
+    }
+  }, [quotes]);
+
   return (
     <>
       <Head>
@@ -12,7 +39,19 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="">asdads</main>
+      <Navbar />
+      <main className="flex flex-col items-center p-8">
+        {currentQuote ? (
+          <Quotecard
+            quote={currentQuote}
+            quotes={quotes}
+            setCurrentQuote={setCurrentQuote}
+            handleCurrentQuote={handleCurrentQuote}
+          />
+        ) : (
+          "loading"
+        )}
+      </main>
     </>
   );
 }
